04077d9ee359307ef61785bf761d8fb1719fdd95
Abdurahim <abdurahim.sobirjonov@email.ru>
2023-07-22 05:16:00 +0500

    ikkinchi commit

--- src/main/java/com/example/customercrud/controller/CustomerController.java
+++ src/main/java/com/example/customercrud/controller/CustomerController.java
@@ -4,11 +4,21 @@ import com.example.customercrud.dto.ApiResponse;
 import com.example.customercrud.dto.CustomerDTO;
 import com.example.customercrud.entity.Customer;
 import com.example.customercrud.service.CustomerService;
+import jakarta.validation.Valid;
 import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
@@ -25,10 +35,11 @@ public class CustomerController {
         return customerById;
     }
     @PostMapping
-    public ApiResponse addCustomer(@RequestBody CustomerDTO customerDTO){
+    public ApiResponse addCustomer(@Valid @RequestBody CustomerDTO customerDTO){
         ApiResponse apiResponse = customerService.saveCustomer(customerDTO);
         return apiResponse;
     }
+
     @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
     public void delete(@PathVariable Integer id){
         customerService.deleteCustomerById(id);
@@ -38,5 +49,19 @@ public class CustomerController {
         ApiResponse apiResponse = customerService.updateCustomer(customerDTO,id);
         return apiResponse;
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
 
 }
+
+
--- src/main/java/com/example/customercrud/dto/CustomerDTO.java
+++ src/main/java/com/example/customercrud/dto/CustomerDTO.java
@@ -1,6 +1,7 @@
 package com.example.customercrud.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class CustomerDTO {
+    @NotNull (message = "Ism kiritiladi")
     private String name;
+    @NotNull(message = "telefon raqam kiritilmadi")
     private String phone;
+    @NotNull(message = "Adres kiritilmadi")
     private String address;
 }
